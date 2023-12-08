ae8fd7ec95c281ea23c9a85b87a7fd051247821d
Alireza <96019816+AlirezaRafiei9@users.noreply.github.com>
2023-12-07 22:50:41 -0500

    Delete Lexicon directory

--- Lexicon/Load_model.py
+++
@@ -1,7 +0,0 @@
-!pip install simpletransformers
-from simpletransformers.classification import ClassificationModel
-
-model = ClassificationModel("bert", "/content/drive/MyDrive/BMI 550/Final_project/BERT_large_model3")
-
-test_text=list(df_test['text'])
-predictions, raw_outputs = model.predict(test_text)
